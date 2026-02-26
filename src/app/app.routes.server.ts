6ccfad3f4e87d7b7af7dd0d43b96879957c15640
menna525 <ma22504787@gmail.com>
2026-02-26 15:27:15 +0200

    edit app.routes.server.ts file

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,21 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+
+  // Dynamic routes → SSR
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'details/:slug/:id',
+    renderMode: RenderMode.Server
+  },
+
+  // Everything else → Prerender
   {
     path: '**',
     renderMode: RenderMode.Prerender
   }
+
 ];
